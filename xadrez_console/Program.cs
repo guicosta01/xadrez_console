044aa48ee5daa12f9db31d40b16572b92dda385c
guicosta01 <guiferreiracosta@hotmail.com>
2024-06-10 19:46:55 -0300

    excessao perso

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -4,9 +4,15 @@ using xadrez_console;
 using xadrez_console.tabuleiro;
 using xadrez_console.xadrez;
 
-Tabuleiro tab = new(8, 8);
-tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+try{
+    Tabuleiro tab = new(8, 8);
+    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
+    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
 
-Tela.imprimirTabuleiro(tab);
+    Tela.imprimirTabuleiro(tab);
+}
+catch (TabuleiroException e){
+    Console.Write(e.Message);
+}
+
